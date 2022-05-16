5b2913cd35dc576ee9b7b32b1f17c793a71a5868
marcelo pimenta <marcelopimenta.work@gmail.com>
2022-05-16 01:57:43 -0400

    separete files

---
+++ createTemplates/create-templates.js
@@ -0,0 +1,24 @@
+export function createTemplate(item) {
+
+    const { name, imageUrl, price, uniqueId } = item
+    const priceTreat = treatPrices(price)
+
+    return `
+       <div class="truffles" id="${uniqueId}">
+            <div class="imagetruffles">
+                <img src="${imageUrl}" alt="">
+            </div>
+            <div class="information">
+                <div>
+                    <h1 class="name-products">${name}</h1>
+                </div>
+                <div>
+                    <span class="price">${priceTreat}</span>
+                </div>
+                <div class="buttons-add-to-cart">
+                    <button class="buttons-action add-to-cart finalize-purchase">Adicionar ao carrinho</button>
+                </div>
+            </div>
+       </div>
+    `
+}
